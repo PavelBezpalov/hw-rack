0c1e38d73b15ef5bf13850aa1887585715de3d12
Artem Kobets <artemkobets01@gmail.com>
2017-10-21 22:10:40 +0300

    Homework

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,22 @@
 module EmailCollector
   class Email
-    # YOUR CODE HERE
+    attr_reader :errors
+
+    def initialize(email)
+      @email_regex = /^[a-z0-9]+@[a-z0-9]+\.[a-z0-9]+$/i
+      @errors = ''
+      @email = email
+    end
+
+    def save
+      if @email =~ @email_regex
+        File.open(EmailCollector::DATABASE, 'a') { |f| f.puts @email }
+        @errors = ''
+        true
+      else
+        @errors = 'Wrong email format.'
+        false
+      end
+    end
   end
 end
