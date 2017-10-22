f941c43a849152ee7517584d527eb0e52df787a5
Oleg <oleg@oleg.pritula>
2017-10-23 01:33:15 +0300

    update help from https://ru.stackoverflow.com/questions/459251/

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,6 +3,7 @@ module EmailCollector
     def self.create(req)
       if req.params.key?('email')
         # YOUR CODE HERE
+        SubscribeMailer.subscribe_email(@subscriber).deliver
       else
         [403, { 'Content-Type' => 'text/plain' }, ['Missing param: email']]
       end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,10 @@
 module EmailCollector
   class Email
     # YOUR CODE HERE
+    validates :email, presence: true, uniqueness: true
+    def subscribe_email(subscriber)
+   @subscriber = subscriber
+   mail(to: @subscriber.email, subject: "New subscriber", cc: "admin@site.com"
+end
   end
 end
