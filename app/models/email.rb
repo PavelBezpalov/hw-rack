a254acac482d990607a2bd140a173757d4414abe
DmitriyDats <datsenkodmitriy10@gmail.com>
2017-10-25 22:07:14 +0300

    first commit

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,25 @@
 module EmailCollector
   class Email
     # YOUR CODE HERE
+    attr_reader :email, :errors
+
+    def initialize(email)
+      @email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      @email = email
+      @errors = ""
+    end
+
+    def save
+      if @email =~ @email_regex
+        File.open(EmailCollector::DATABASE, "a") {|file| file.puts @email}
+        @errors = ""
+        true
+      else
+        @errors = "Wrong email format."
+        false
+      end
+    end
+
   end
+
 end
