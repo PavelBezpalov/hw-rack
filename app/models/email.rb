1b1bbd0ac55d313a2e97e914f224010dab5400b3
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2017-11-05 20:34:08 +0000

    Implement Email model

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,23 @@
 module EmailCollector
   class Email
-    # YOUR CODE HERE
+    def initialize(address)
+      @address = address
+    end
+    
+    def errors
+      @errors || ''
+    end
+    
+    def save
+      if @address == ''
+        @errors = 'Wrong email format.'
+        false
+      else
+        File.open(EmailCollector::DATABASE, 'a') do |file|
+          file.puts @address
+        end
+        true
+      end
+    end
   end
 end
