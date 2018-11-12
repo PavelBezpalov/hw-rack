9bc2b5659c3e3eb612d44ca7cd305629770f17f4
MaksGostroushko <32840948+MaksGostroushko@users.noreply.github.com>
2018-11-12 11:42:58 +0200

    Update application.rb

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+require './app/lib/renderable'
 require './app/controllers/emails_controller'
 require './app/controllers/errors_controller'
 require './app/controllers/pages_controller'
@@ -5,12 +6,13 @@ require './app/models/email'
 
 module EmailCollector
   DATABASE = './db/email_addresses.txt'.freeze
+  EMAIL_REGEX = /(.+)@(.+)\.(.+)/i
 
   class Application
     def self.call(env)
       req = Rack::Request.new(env)
       return PagesController.root             if req.get?  && req.path == '/'
-      # YOUR CODE HERE
+      return EmailsController.create(req)     if req.post? && req.path == '/emails'
       ErrorsController.not_found
     end
   end
